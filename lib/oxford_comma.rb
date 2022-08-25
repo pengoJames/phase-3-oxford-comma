7dd39ce010d59ee760d14442e7de3967acc8de5e
Pe-ngo <jamespengo95@gmail.com>
2022-08-25 08:25:21 +0300

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
         array.join
     elsif array.length == 2
         array.join(" and ")
-    else
+    elsif array.length >= 3
         array[-1] = "and " << array[-1]
         array.join(", ")
     end
