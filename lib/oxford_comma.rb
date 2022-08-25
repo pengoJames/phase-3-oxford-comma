338d6eda8e1da0fe9c1e359d67e6b45294a26556
Pe-ngo <jamespengo95@gmail.com>
2022-08-25 08:14:54 +0300

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        array[-1] = "and " << array[-1]
+        array.join(", ")
+    end
 end
+p oxford_comma(["fiddleheads", "okra", "kohlrabi"])
